292ea9b8d9929961c023bf1f0b170be0cc6783ac
AlejandroJimeneRuiz <pnf88400@educastur.es>
2025-01-23 12:52:42 +0100

    Card Display
    Card Display script WIP

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,23 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
  [System.Serializable]
 
-public class Card_Info : MonoBehaviour
+public class Card
 {
     public int id;
-    public string cardName;
+    public TextMeshProUGUI cardName;
     public int cardHp;
     public int cost;
     public int power;
-    public string cardDescription;
+    public TextMeshProUGUI cardDescription;
 
-    public void Card()
-    {
 
-    }
-
-    public void Card(int Id, string CardName, int CardHp, int Cost, int Power, string CardDescription)
+    public Card(int Id, TextMeshProUGUI CardName, int CardHp, int Cost, int Power, TextMeshProUGUI CardDescription)
     {
         id = Id;
         cardName = CardName;
@@ -28,5 +25,9 @@ public class Card_Info : MonoBehaviour
 
 
 
+    }
+
+    public Card(int v1, string v2, int v3, int v4, int v5, string v6)
+    {
     }
 }
---
+++ Assets/Scripts/Display_Card.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+public class Display_Card : MonoBehaviour
+{
+    public List<Card> displayCard = new List<Card>();
+    public int displayId;
+
+    public int id;
+    public TextMeshProUGUI cardName;
+    public int cost;
+    public int power;
+    public TextMeshProUGUI cardDescription;
+    public int hp;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI costText;
+    public TextMeshProUGUI powerText;
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI hpText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayCard[0] = Card_DataBase.cardList[displayId];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        id = displayCard[0].id;
+        cardName = displayCard[0].cardName;
+        cost = displayCard[0].cost;
+        power = displayCard[0].power;
+        cardDescription = displayCard[0].cardDescription;
+        hp = displayCard[0].cardHp;
+    
+        nameText.text = " " + cardName;
+        costText.text = " " + cost;
+        powerText.text = " " + power;
+        descriptionText.text = " " + cardDescription;
+        hpText.text = " " + hp;
+    }
+}
