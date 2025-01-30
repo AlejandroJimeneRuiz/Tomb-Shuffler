d23a5254706bdd6225caf9a562bbce858659d1bd
AlejandroJimeneRuiz <pnf88400@educastur.es>
2025-01-30 11:50:40 +0100

    Shuffle (WIP)

--- Assets/Scripts/Card_DataBase.cs
+++ Assets/Scripts/Card_DataBase.cs
@@ -9,12 +9,11 @@ public class Card_DataBase : MonoBehaviour
 
     void Awake()
     {
-        cardList.Add(new Card(0, "None", 0, 0, 0, "None", Resources.Load<Sprite>("Placeholder")));
-        cardList.Add(new Card(1, "Test", 1, 1, 1, "This is a test card.", Resources.Load<Sprite>("Placeholder")));
-        cardList.Add(new Card(2, "Human", 1, 1, 1, "This is a Human.", Resources.Load<Sprite>("Placeholder")));
-        cardList.Add(new Card(3, "Elf", 1, 1, 1, "This is a Elf.", Resources.Load<Sprite>("Placeholder")));
-        cardList.Add(new Card(4, "Dwarf", 2, 2, 1, "This is a Dwarf.", Resources.Load<Sprite>("Placeholder")));
-        cardList.Add(new Card(5, "Troll", 3, 3, 3, "This is a Troll.", Resources.Load<Sprite>("Placeholder")));
+        cardList.Add(new Card(0, "Test", 1, 1, 1, "This is a test card.", Resources.Load<Sprite>("Placeholder")));
+        cardList.Add(new Card(1, "Human", 1, 1, 1, "This is a Human.", Resources.Load<Sprite>("Placeholder")));
+        cardList.Add(new Card(2, "Elf", 1, 1, 1, "This is a Elf.", Resources.Load<Sprite>("Placeholder")));
+        cardList.Add(new Card(3, "Dwarf", 2, 2, 1, "This is a Dwarf.", Resources.Load<Sprite>("Placeholder")));
+        cardList.Add(new Card(4, "Troll", 3, 3, 3, "This is a Troll.", Resources.Load<Sprite>("Placeholder")));
 
     }
 }
