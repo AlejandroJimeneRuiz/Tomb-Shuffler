86202e1c5d4d23786d2008955f4713d55c084cbf
NereaCoppen <nereacf94@educastur.es>
2025-03-06 20:36:35 +0100

    Nuevos scripts y cambios de interaccion respecto a dialogo

---
+++ Assets/Scripts/Puzzle/AltarTrigger.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class AltarTrigger : MonoBehaviour
+{
+    public TextMeshProUGUI textoDialogo;
+    public string[] dialogos = {
+        "Press F to stop move",
+        "Press ← to select nut",
+        "Press E to rotate",
+        "Press F to move"
+    };
+    private bool mostrandoDialogo = false;
+
+    void Start()
+    {
+        // Asegúrate de que el texto esté oculto al inicio
+        if (textoDialogo != null)
+        {
+            textoDialogo.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !mostrandoDialogo)
+        {
+            StartCoroutine(MostrarDialogo());
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Ocultar el texto si el jugador sale del trigger
+            if (textoDialogo.gameObject.activeSelf)
+            {
+                textoDialogo.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    IEnumerator MostrarDialogo()
+    {
+        mostrandoDialogo = true;
+        textoDialogo.gameObject.SetActive(true);
+
+        foreach (string linea in dialogos)
+        {
+            textoDialogo.text = linea;
+            yield return new WaitForSeconds(2f);
+        }
+
+        textoDialogo.gameObject.SetActive(false);
+        mostrandoDialogo = false;
+    }
+}
+
