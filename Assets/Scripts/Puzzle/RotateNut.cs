1d2e0a699b8dafb229449a03a1e03a09a29b81ea
NereaCoppen <nereacf94@educastur.es>
2025-03-07 09:37:00 +0100

    cambios
    Puerta derecha a noventa grados en Y para que abra bien
    Cambios en el script rotatenut
    eliminacion de interaccion en la camara (no se necesita para el puzzle gracias a otro script que tiene)

--- Assets/Scripts/Puzzle/RotateNut.cs
+++ Assets/Scripts/Puzzle/RotateNut.cs
@@ -6,14 +6,34 @@ public class RotateNut : MonoBehaviour
 {
     private int currentRotation = 0;
     private int requiredRotation = 0; // Rotación requerida para resolver el puzzle
+    private bool isRotating = false; // Variable para controlar si está rotando
 
     public void Rotate90Degrees()
     {
-        if (currentRotation != requiredRotation)
+        if (currentRotation != requiredRotation && !isRotating)
         {
-            transform.Rotate(0, 90, 0);
-            currentRotation = (currentRotation + 1) % 4; // Incrementa y mantiene el valor entre 0 y 3
+            StartCoroutine(RotateSmoothly());
         }
+    }
+
+    private IEnumerator RotateSmoothly()
+    {
+        isRotating = true;
+        float elapsedTime = 0f;
+        float rotationDuration = 0.5f; // Duración de la rotación en segundos
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = transform.rotation * Quaternion.Euler(0, 90, 0);
+
+        while (elapsedTime < rotationDuration)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsedTime / rotationDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = endRotation;
+        currentRotation = (currentRotation + 1) % 4; // Incrementa y mantiene el valor entre 0 y 3
+        isRotating = false;
     }
 
     public int GetCurrentRotation()
@@ -31,3 +51,4 @@ public class RotateNut : MonoBehaviour
         return requiredRotation;
     }
 }
+
